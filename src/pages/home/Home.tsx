e802e5d19fe2a854ce458484ace3420a92eeebf0
Julia Domingues <142114663+julooch@users.noreply.github.com>
2023-11-14 09:49:23 -0300

    Desenvolvimento do CardPostagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
-function Home() {
+import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem"
 
+function Home() {
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
@@ -9,13 +11,12 @@ function Home() {
                             Seja Bem Vinde!
                         </h2>
                         <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
+                            Expresse aqui seus pensamentos e opiniões
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white border-white border-solid border-2 py-2 px-4'
-                            >
-                                Nova Postagem
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
                             </div>
                         </div>
                     </div>
@@ -29,6 +30,8 @@ function Home() {
                     </div>
                 </div>
             </div>
+
+            <ListaPostagens />
         </>
     )
 }
